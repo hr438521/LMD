a75a36c3eab9bfaaa41302f2c2e6d137689f6812
renr <123444@aliyun.com>
2019-06-24 21:53:43 +0800

    利民贷

---
+++ src/main/java/com/qfedu/lmd/lmd/LmdApplication.java
@@ -0,0 +1,13 @@
+package com.qfedu.lmd.lmd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LmdApplication.class, args);
+    }
+
+}
